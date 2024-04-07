0d81bb6ad83e5932943cec1f8b4d23d4519cc42c
Gaze <dev@gaze.network>
2024-04-07 16:19:28 +0700

    feat: add Uint64() and Uint32() utility functions

--- uint128.go
+++ uint128.go
@@ -32,6 +32,16 @@ func (u Uint128) IsZero() bool {
 	return u == Uint128{}
 }
 
+// Uint64 returns the lower 64 bits of u.
+func (u Uint128) Uint64() uint64 {
+	return u.Lo
+}
+
+// Uint32 returns the lower 32 bits of u.
+func (u Uint128) Uint32() uint32 {
+	return uint32(u.Lo)
+}
+
 // Equals returns true if u == v.
 //
 // Uint128 values can be compared directly with ==, but use of the Equals method
